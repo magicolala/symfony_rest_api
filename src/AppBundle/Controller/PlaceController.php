ff7a36cfddb477f67743fd97ae48ac65cda23573
Cédric Oloa <cedric.oloa@laposte.net>
2017-04-24 23:35:54 +0200

    Form Symfony

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -9,7 +9,7 @@ use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandler;
-
+use AppBundle\Form\Type\PlaceType;
 use AppBundle\Entity\Place;
 
 class PlaceController extends Controller
@@ -45,5 +45,88 @@ class PlaceController extends Controller
         }
 
         return $place;
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/places")
+     */
+    public function postPlacesAction(Request $request)
+    {
+        $place = new Place();
+        $form = $this->createForm(PlaceType::class, $place);
+
+        $form->submit($request->request->all()); // Validation des données
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($place);
+            $em->flush();
+            return $place;
+        } else {
+            return $form;
+        }
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
+     * @Rest\Delete("/places/{id}")
+     */
+    public function removePlaceAction(Request $request)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $place = $em->getRepository('AppBundle:Place')
+            ->find($request->get('id'));
+        /* @var $place Place */
+
+        if ($place) {
+            $em->remove($place);
+            $em->flush();
+        }
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Put("/places/{id}")
+     */
+    public function updatePlaceAction(Request $request)
+    {
+        return $this->updatePlace($request, true);
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Patch("/places/{id}")
+     */
+    public function patchPlaceAction(Request $request)
+    {
+        return $this->updatePlace($request, false);
+    }
+
+    private function updatePlace(Request $request, $clearMissing)
+    {
+        $place = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:Place')
+            ->find($request->get('id')); // L'identifiant en tant que paramètre n'est plus nécessaire
+        /* @var $place Place */
+
+        if (empty($place)) {
+            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $form = $this->createForm(PlaceType::class, $place);
+
+        // Le paramètre false dit à Symfony de garder les valeurs dans notre
+        // entité si l'utilisateur n'en fournit pas une dans sa requête
+        $form->submit($request->request->all(), $clearMissing);
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($place);
+            $em->flush();
+            return $place;
+        } else {
+            return $form;
+        }
     }
 }
